8918200c97e3d62d39b95cbfd176033d212f7961
prkiv6666 <pepsi213@abv.bg>
2025-03-05 19:50:18 +0200

    Add files via upload
    Добавяне на първоначална имплементация на класа Book с базови атрибути за Системата за библиотека

---
+++ LibrarySystem.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Book {
+    private String author;
+    private String title;
+    private String genre;
+    private String description;
+    private int year;
+    private List<String> tags;
+    private double rating;
+    private int isbn;
+}
